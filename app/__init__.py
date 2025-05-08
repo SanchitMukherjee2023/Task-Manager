30d8567e1832134d1a6b554ead2a7d97aeeb6cfb
xSanchitMx <mukherjee.sanchit@gmail.com>
2025-05-08 20:00:30 +0530

    Adding wsgi

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
 
-    from app import routes
-    app.register_blueprint(routes.bp)
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from .routes import main
+    app.register_blueprint(main)
 
     return app
