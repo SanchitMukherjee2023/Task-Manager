398a192858f4a3c9438bed732cdffb591c790dbe
xSanchitMx <mukherjee.sanchit@gmail.com>
2025-05-08 20:16:02 +0530

    4

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
 
+    login_manager.login_view = 'main.login'  # 👈 Add this line!
+
     from .routes import main
     app.register_blueprint(main)
 
